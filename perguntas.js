faecb67c3a7903e60b129df966b024ae936699e7
rhaissafonseca2009 <rhaissa.fonseca@escola.pr.gov.br>
2025-07-07 08:42:11 -0300

    rrrrr

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+perguntas js
+criaCartao(
+    'Moda inclusiva e Diversidade',
+    'O que significa "diversidade" na indústria da moda?',
+    'Na indústria da moda, diversidade significa representar e incluir pessoas de diferentes etnias, tamanhos, idades, gêneros, origens culturais e habilidades, quebrando padrões estreitos de beleza e permitindo que todos se sintam vistos e valorizados.'
+)
+
+criaCartao(
+    'Moda Inclusiva: Estilo Para Todos',
+    'Como a moda pode ser mais inclusiva para pessoas de diferentes tipos de corpo?',
+    'A moda pode ser mais inclusiva oferecendo uma variedade de tamanhos, modelagens ajustadas, tecidos adaptáveis, campanhas com modelos de diferentes tipos de corpo e criando peças versáteis que atendam a todas as formas, garantindo que todos se sintam representados e confortáveis.'
+)
+
+criaCartao(
+    'Inclusão na Passarela',
+    'Como a moda pode ser mais inclusiva para pessoas de diferentes tipos de corpo?',
+    'A moda pode ser mais inclusiva para diferentes tipos de corpo ao oferecer uma ampla variedade de tamanhos, usar modelagens que se ajustem melhor a diferentes formas, criar peças versáteis e confortáveis com tecidos adaptáveis, e representar essa diversidade em campanhas e desfiles, mostrando que estilo e beleza não têm um único padrão.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)

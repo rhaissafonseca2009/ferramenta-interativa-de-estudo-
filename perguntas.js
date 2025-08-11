73e9706755f9e7f07fd0a461e261f5b69b3e9180
rhaissafonseca2009 <rhaissa.fonseca@escola.pr.gov.br>
2025-08-11 08:34:46 -0300

    ycfy

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,22 @@
-perguntas js
 criaCartao(
-    'Moda inclusiva e Diversidade',
-    'O que significa "diversidade" na indústria da moda?',
-    'Na indústria da moda, diversidade significa representar e incluir pessoas de diferentes etnias, tamanhos, idades, gêneros, origens culturais e habilidades, quebrando padrões estreitos de beleza e permitindo que todos se sintam vistos e valorizados.'
+    'Perguntas frequentes'
+    'o que e autismo',?',
+    'Autismo é uma condição que afeta a forma como uma pessoa se comunica, interage e percebe o mundo ao seu redor.'
 )
 
 criaCartao(
-    'Moda Inclusiva: Estilo Para Todos',
-    'Como a moda pode ser mais inclusiva para pessoas de diferentes tipos de corpo?',
-    'A moda pode ser mais inclusiva oferecendo uma variedade de tamanhos, modelagens ajustadas, tecidos adaptáveis, campanhas com modelos de diferentes tipos de corpo e criando peças versáteis que atendam a todas as formas, garantindo que todos se sintam representados e confortáveis.'
+    // 'autismo perguntas frequentes',
+    'Quais são os sinais do autismo?',
+    'Alguns sinais incluem dificuldades na comunicação, interesses restritos, comportamentos repetitivos e dificuldades em interagir com outras pessoas.'
 )
 
 criaCartao(
-    'Inclusão na Passarela',
-    'Como a moda pode ser mais inclusiva para pessoas de diferentes tipos de corpo?',
-    'A moda pode ser mais inclusiva para diferentes tipos de corpo ao oferecer uma ampla variedade de tamanhos, usar modelagens que se ajustem melhor a diferentes formas, criar peças versáteis e confortáveis com tecidos adaptáveis, e representar essa diversidade em campanhas e desfiles, mostrando que estilo e beleza não têm um único padrão.'
+    'autismo perguntas frequentes',
+    'Como ajudar uma pessoa com autismo?',
+    'Oferecendo compreensão, paciência, apoio na comunicação e participando de terapias específicas.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
+    'autismo perguntas frequentes',
+    'Existe cura para o autismo?',
+    'Não há cura, mas com apoio e terapias, as pessoas com autismo podem desenvolver suas habilidades e ter uma vida plena.'
